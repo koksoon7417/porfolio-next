021a1da09ae9105cce752d3acd4e1207bdd66e07
Kok Soon <koksoon7417@gmail.com>
2023-09-03 23:53:06 +0800

    Deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     output: 'export',
     basePath: '/porfolio-next',
     assetPrefix: '/porfolio-next/',
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
