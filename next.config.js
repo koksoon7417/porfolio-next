a0f6b971265ec5674ee3c6e98de2b6bb7199018b
Kok Soon <koksoon7417@gmail.com>
2023-09-04 22:35:36 +0800

    fix pathname when empty

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: `${process.env.exportPath}`,
-    assetPrefix: `${process.env.exportPath}/`,
-    images: {
-        unoptimized: true,
-    },
+  output: 'export',
+  basePath: process.env.exportPath || '',
+  assetPrefix: process.env.exportPath ? `${process.env.exportPath}/` : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
